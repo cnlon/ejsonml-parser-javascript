ca41a68e5bbc019bbce96ff26d488de47b6ac2b9
longhao <longhaohe@gmail.com>
2016-06-24 22:46:32 +0800

    Rewrite

--- dev/index.js
+++ dev/index.js
@@ -1,17 +1,28 @@
-import parse from '../lib'
+import Parser from '../../ejsonml-parser/lib/index.js'
+import JmlParser from '../lib'
 
+const parser = new Parser()
 window.onload = function () {
+  const jmlParser = new JmlParser({
+    callback (jml) {
+      const resNode = document.getElementById('jml')
+      resNode.innerText = JSON.stringify(jml, null, 2)
+    },
+  })
+  parser.install(jmlParser)
+
   const template =
   `
-  <div class="className" style="background-color: red;" :style="style">
-    <strong :text="name+','"></strong>你好
+  <div *if="a" *for="a in c" class="className" style="background-color: red;" :style="style">
+    <strong :text="name+',' + a">a</strong>你好
+    <i></i>
     <!--comment!!-->
   </div>
   `
   const tplNode = document.getElementById('tpl')
   tplNode.innerText = template
 
-  const ejml = parse(template)
-  const resNode = document.getElementById('res')
-  resNode.innerText = JSON.stringify(ejml, null, '  ')
+  const ejml = parser.parse(template)
+  const ejmlNode = document.getElementById('ejml')
+  ejmlNode.innerText = JSON.stringify(ejml, null, 2)
 }
